6abf0c23dfc967410cd0dc591f191f09944c247f
vchambers6 <35242014+vchambers6@users.noreply.github.com>
2020-02-19 20:31:10 -0500

    Update randmst.cpp
    2/19 8:30 pm
    Matrix works

--- randmst.cpp
+++ randmst.cpp
@@ -1,12 +1,13 @@
 
 #include <cstdlib>
+#include <cstdio> 
 // cin << nump << numt << dim;
 int main() {
 int n = 2;
 double mat [n][n];
 
-for (int i; i < n; i++) {
-    for (int j; j < n; j++) {
+for (int i; i <= n; i++) {
+    for (int j; j <= n; j++) {
       /* random algorithm taken from: https://l.facebook.com/l.php?u=https%3A%2F
       2Fstackoverflow.com%2Fquestions%2F6218399%2Fhow-to-generate-a-random-numbe
       r-between-0-and-1%3Ffbclid%3DIwAR3r1JVNyAvtTWXeNXs2ti89zELUQWnMzJYK1_l3kP
@@ -14,6 +15,8 @@ for (int i; i < n; i++) {
       nRnW8iuL65bbVPBm9QDDSIdGxqSuTwNU4GLva6hR4AhsflrZ9T_G01zGK41t-PLC5A5hhMTtuf
       NoXbjQ */
       mat[i][j] = (double)rand() / (double)RAND_MAX;
+      printf("%lf \n", mat[i][j]);
     }
 }
+
 }
