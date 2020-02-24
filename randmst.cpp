134ed5331183eab922982fdd413737c18fae0b49
vchambers6 <35242014+vchambers6@users.noreply.github.com>
2020-02-23 20:56:29 -0500

    Starting Fresh
    Java and python files added

--- randmst.cpp
+++ randmst.cpp
@@ -3,6 +3,9 @@
 #include <cstdio>
 #include <vector>
 #include <cmath>
+#include <iostream>
+#include <tuple>
+#include <functional>
 //#include <bits/stdc++.h>
 //what exactly are you using from this header file? i dont think we should include this
 //because it messes with the runtime for our program 
@@ -11,7 +14,7 @@ using namespace std;
 
 // Function to calculate distance between two points
 // Found on https://www.geeksforgeeks.org/program-calculate-distance-two-points/
-float two_distance(int x1, int y1, int x2, int y2)
+float two_distance(double x1, double y1, double x2, double y2)
 {
     // Calculating distance
     return sqrt(pow(x2 - x1, 2) +
@@ -19,11 +22,21 @@ float two_distance(int x1, int y1, int x2, int y2)
 }
 
 // Function to calculate distance between three points
-float three_distance(int x1, int y1, int z1, int x2, int y2, int z2)
+float three_distance(double x1, double y1, double z1, double x2, double y2, double z2)
 {
     // Calculating distance
     return sqrt(pow(x2 - x1, 2) +
                 pow(y2 - y1, 2) + pow(z2 - z1, 2) * 1.0);
+}
+
+float redo_x(double x1, double x2, double y1, double y2) {
+	if (x1 == x2 && y1 == y2) {
+		x2 = (double)rand() / (double)RAND_MAX;
+		redo_x(x1, x2)
+	}
+	else {
+		return x2 
+	}
 }
 
 double * one_dim(int n) {
@@ -45,25 +58,40 @@ double * one_dim(int n) {
 
 double * two_dim(int n) {
   // Creates vector to keep track of vertex coordinates
-  vector<int> V;
+  /*std::tuple <double, double> val
+  std::vector<val> V ; */
   //Initiliza distance array
   double mat [n][n];
   // Fill Vertex vector with random tuples
-  for (int i; i<= n; i++) {
+  /*for (int i; i<= n; i++) {
 	double x = (double)rand() / (double)RAND_MAX; 
     double y = (double)rand() / (double)RAND_MAX;
-    V.push_back((x,y));
+    V.push_back(std::make_tuple(x,y));
+  }
+  */
+  //initialize all entries in matrix to 0 
+  for (int i = 0; i<n; i++){
+  	for (int j=0; j < n; j++){
+  		mat[i][j] = 0.0;  
+  	}
+  	
   }
 
-  for (int i; i <= n; i++) {
-      for (int j; j <= n; j++) {
-        mat[i][j] = two_distance(V[i][0],V[j][0],V[i][1],V[j][1]);
+  for (int i=0; i < n; i++) {
+      for (int j=0; j < n; j++) {
+      	if ((i != j) && (mat[i][j] ))
+      	double x1 = (double)rand() / (double)RAND_MAX; ;
+      	double y1 = (double)rand() / (double)RAND_MAX; ;
+      	double x2 = (double)rand() / (double)RAND_MAX; ;
+      	double y2 = (double)rand() / (double)RAND_MAX; ;
+      	x2 = redo_x(x1, x2, y1, y2); 
+        mat[i][j] = two_distance(x1,y1,x2,y2);
       }
     }
     return *mat;
 }
 
-double* three_dim(int n) {
+/*double* three_dim(int n) {
   // Creates vector to keep track of vertex coordinates
   vector<int> V;
   //Initiliza distance array
@@ -84,13 +112,13 @@ double* three_dim(int n) {
   }
   return mat;
 }
-
+*/
 // cin << nump << numt << dim;
 int main() {
 int n = 2;
 one_dim(n); 
 two_dim(n); 
-three_dim(n); 
+//three_dim(n); 
 }
 
 
---
+++ randmst.py
@@ -0,0 +1,19 @@
+import random
+
+#random number generator on range [0.0, 1) 
+random.random()
+
+#declare set of vertices
+vertices = set()
+
+#initialize set of vertices
+while len(vertices) < n:
+	vertices.add([random.random(), random.random()])
+
+edges = {}
+
+for i in range(n):
+	for j in range(n): 
+	edges = {vertices(i): }
+
+
