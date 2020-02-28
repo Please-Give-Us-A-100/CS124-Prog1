7b8f161195512fc7233fc9ffe60313b2444218d4
vchambers6 <35242014+vchambers6@users.noreply.github.com>
2020-02-28 13:35:28 -0500

    Update randmst.cpp
    Works for all dimensions.
    Cleaner code, better design.

--- randmst.cpp
+++ randmst.cpp
@@ -15,38 +15,36 @@ struct Point{
 	float weight; 
 }; 
 
-float distance(double x1, double y1, double z1, double a1, double x2, double y2, double z2, double a2){
+float distance(float x1, float y1, float z1, float a1, float x2, float y2, float z2, float a2){
 	return sqrt(pow(x2 - x1, 2) +
                 pow(y2 - y1, 2) + pow(z2 - z1, 2) + pow(a2 - a1, 2)* 1.0); 
 }
 
 float prim(int n, int d) { 
-	std::time_t start_time = std::time(0); 
 	// Array of vertices  
 	struct Point arr[n]; 
-	 
-	// Resets random number generator 
-	srand(time(NULL));
+	
 	// Initializes array
 	for(int i = 0; i < n; i++) {
 		arr[i].visited = false; 
+		// Weight initialized to overestimate of max edge length 
 		arr[i]. weight = 50.0;
 		arr[i].x = 0.0;
 		arr[i].y = 0.0; 
 		arr[i].z = 0.0; 
 		arr[i].a = 0.0; 
 		// Initializes coordinates for higher dimensions 
-		if (d == 2) {
+		if (d >= 2) {
 			arr[i].x = (float)rand() / RAND_MAX;
 			arr[i].y = (float)rand() / RAND_MAX;
-			if (d == 3) {
+			if (d >= 3) {
 				arr[i].z = (float)rand() / RAND_MAX;
 				if (d == 4) {
 					arr[i].a = (float)rand() / RAND_MAX;
 				}
 			}
 		}
-		//arr[i]. weight = std::numeric_limits<float>::max();;
+		
 	}
 
 	int edges; 
@@ -61,62 +59,68 @@ float prim(int n, int d) {
 	while (edges < n) {
 		arr[niq].visited = true; 
 		for(int i = 0; i < n ; i ++){
+			// Checks if vertex has been visited
 			if (arr[i].visited == false) {
-				//cout << "node" << i << "visited \n"; 
 				if (d == 0) {
+					// Edge weight calculated for 0 dimension
 					dist = (float)rand() / RAND_MAX; 
 				}
 				else {
+					// Edge weight calculated for higher dimensions
 					dist = distance(arr[i].x, arr[i].y, arr[i].z, arr[i].a, 
 					arr[niq].x, arr[niq].y, arr[niq].z, arr[niq].a); 
 				}
-				//cout << "distance between " << niq << " and " << i << " is " << dist << "\n";
+				// Updates edge weight to minimum distance
 				if (dist < arr[i].weight)
 					arr[i].weight = dist; 
 				else 
 					dist = arr[i].weight; 
-
+				// Finds smallest edge from node in question
 				if (dist < smallest || smallest == 20.0) {
 					smallest = dist;
 					smallest_index = i; 
-					//cout << "min found at node " << i << "\n"; 
 				}  		
 			} 
 		} 
 		niq = smallest_index; 		 
 		edges++; 
+		// Add smallest edge to MST 
 		if (smallest != 20.0) {
 			total = total + smallest;
-			//cout <<"node in question " << niq << "\nadd to total \n" << "smallest " << smallest << "\n"<< total << "\n" << edges << "\n" << "niq is: " << niq << "\n" << "niq weigjt: " << arr[niq].weight << "\n";
 			smallest = 20.0; 
 		}		
 	}
-	std::time_t end_time = std::time(0); 
-	cout << "time spent " << end_time - start_time << "\n"; 
+	//cout << "time spent: " << end_time - start_time << "\n" << "total " << total << "\n";
+	cout << "WEIGHT MST: " << total << "\n"; 
 	return total; 
 } 
 
-int main(int argc, char *argv[]) {
-	//if (argc != 3) return -1;
-	int n, t, d; 
 
+
+int main(int argc, char *argv[]) {
+	srand(time(NULL));
+
+	int n, t, d; 
+	
     n = atoi(argv[2]);
     t = atoi(argv[3]);
     d = atoi(argv[4]); 
-   // cout << n <<  " trials: " << t << " dim: "<< d << "\n"; 
-    //cin >> n >> t >> d; 
-	//int trials = 5; 
 
-	/*float tot; 
-	for (int i = 0; i < trials; i++){
-		tot += prim_2d(n); 
-	}
-	float avg = tot/trials; 
-	cout << "avergae \n" << avg <<"\n"; */
-	//int a = 1024; 
-	//int b = 2; 
-	float sum = prim(n, d); 
-	cout << "sum: " << sum << "\n";
-	//printf("final %f\n", sum);
+    float sum; 
+    
+    float c, f, e; 
+    std::time_t start_time = std::time(0); 
+    for (int p = 0; p < t; p++){
+    	float * a = (float *) malloc(sizeof(float));
+    	*a = prim(n ,d); 
+    	sum = sum + *a; 
+    	free(a); 
+    }
+    std::time_t end_time = std::time(0);
+
+    float time = (float) (end_time - start_time) / 5.0; 
+    
+    cout << "AVG: " << sum/5.0 << "\n" << "AVG TIME: " << time << "\n"; 
+    
 	return 0; 
 }
